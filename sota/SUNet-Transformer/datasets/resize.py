8f29fac89f7751624ef415739012f9884e5df2bb
Sheikh Shah Mohammad Motiur Rahman <motiur.ion@gmail.com>
2022-06-22 17:28:37 +0200

    --transformer SUNet test

--- sota/SUNet-Transformer/datasets/resize.py
+++ sota/SUNet-Transformer/datasets/resize.py
@@ -2,11 +2,17 @@ from PIL import Image
 import os, sys
 
 
-image = Image.open(sys.argv[1])
-filename = sys.argv[2]
-image = image.resize((256,256),Image.ANTIALIAS)
-image.save(fp=filename)
+#image = Image.open(sys.argv[1])
+#filename = sys.argv[2]
+#image = image.resize((256,256),Image.ANTIALIAS)
+#image.save(fp=filename)
 
+#from PIL import Image, ImageOps
+
+image = Image.open(sys.argv[1])
+box = (256, 256, 511.5, 511.5)
+img2 = image.crop(box)
+img2.save(sys.argv[2])
 
 #Resizes an image and keeps aspect ratio. Set mywidth to the desired with in pixels.
 

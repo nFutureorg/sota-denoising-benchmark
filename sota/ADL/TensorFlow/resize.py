5325f14cf4b2592f5bb488c0e0d95eb16420e87c
Sheikh Shah Mohammad Motiur Rahman <motiur.ion@gmail.com>
2022-06-29 12:08:53 +0200

    --image resize added

---
+++ sota/ADL/TensorFlow/resize.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import os, sys
+
+
+image = Image.open(sys.argv[1])
+filename = sys.argv[2]
+image = image.resize((512,512),Image.ANTIALIAS)
+image.save(fp=filename)
+
+#from PIL import Image, ImageOps
+
+#image = Image.open(sys.argv[1])
+#box = (256, 256, 600, 600)
+#img2 = image.crop(box)
+#img2.save(sys.argv[2])
+
+#Resizes an image and keeps aspect ratio. Set mywidth to the desired with in pixels.
+
+#import PIL
+#from PIL import Image
+
+#mywidth = 300
+
+#img = Image.open('someimage.jpg')
+#wpercent = (mywidth/float(img.size[0]))
+#hsize = int((float(img.size[1])*float(wpercent)))
+#img = img.resize((mywidth,hsize), PIL.Image.ANTIALIAS)
+#img.save('resized.jpg')

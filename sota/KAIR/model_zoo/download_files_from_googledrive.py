2ec0918943426be2d39e67dadc8fc854a8b38608
Sheikh Shah Mohammad Motiur Rahman <motiur.ion@gmail.com>
2022-07-23 14:49:16 +0200

    Update download_files_from_googledrive.py

--- sota/KAIR/model_zoo/download_files_from_googledrive.py
+++ sota/KAIR/model_zoo/download_files_from_googledrive.py
@@ -1,5 +1,5 @@
-import gdown
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
-url = 'https://drive.google.com/drive/folders/13kfr3qny7S2xwG9h7v95F5mkWs0OmU0D?usp=sharing'
-output = 'model_zoo.zip'
-gdown.download(url, output, quiet=False)
+gdd.download_file_from_google_drive(file_id='13kfr3qny7S2xwG9h7v95F5mkWs0OmU0D',
+                                    dest_path='model_zoo.zip',
+                                    unzip=True)

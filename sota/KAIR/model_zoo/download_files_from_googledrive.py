581f90d5a04f89189e9bc81ec0344d14cb7d3080
Sheikh Shah Mohammad Motiur Rahman <motiur.ion@gmail.com>
2022-07-23 15:05:45 +0200

    testset added

--- sota/KAIR/model_zoo/download_files_from_googledrive.py
+++
@@ -1,5 +0,0 @@
-from google_drive_downloader import GoogleDriveDownloader as gdd
-
-gdd.download_file_from_google_drive(file_id='13kfr3qny7S2xwG9h7v95F5mkWs0OmU0D',
-                                    dest_path='model_zoo.zip',
-                                    unzip=True)

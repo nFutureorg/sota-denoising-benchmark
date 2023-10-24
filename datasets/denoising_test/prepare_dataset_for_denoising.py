137c54351822be01d0156d92b5a079f7d617d0a8
Sheikh Shah Mohammad Motiur Rahman <motiur.ion@gmail.com>
2023-10-24 14:32:03 +0200

    denoising_test

---
+++ datasets/denoising_test/prepare_dataset_for_denoising.py
@@ -0,0 +1,122 @@
+import os
+import cv2
+import numpy as np
+import sys
+from pathlib import Path
+import random
+
+def add_gamma_noise_to_images(input_folder, output_folder_clean, output_folder_noisy, scale_parameters):
+    """
+    Read images from input_folder, add gamma-distributed noise with different scale parameters,
+    and save noisy and clean versions in output_folder_noisy and output_folder_clean, respectively.
+
+    Parameters:
+    - input_folder: Path to the folder containing input images.
+    - output_folder_clean: Path to the folder where clean images will be saved.
+    - output_folder_noisy: Path to the folder where noisy images will be saved.
+    - scale_parameters: List of scale parameters (θ) for the gamma distribution.
+    """
+    # Create output folders if they don't exist
+    os.makedirs(output_folder_clean, exist_ok=True)
+    os.makedirs(output_folder_noisy, exist_ok=True)
+
+    # List all files in the input folder
+    image_files = list_image_files(input_folder)
+    y=0
+    x=0
+    h=600
+    w=1024
+    #print("Gamma Noise")
+    for image_file in image_files:
+        if image_file.endswith(('.jpg', '.jpeg', '.png')):
+            # Read the image
+            #image_path = os.path.join(input_folder, image_file)
+            #print(image_file)
+            image = cv2.imread(image_file)
+            crop_img_clean = image[y:y+h, x:x+w]
+            #image = crop_img_clean.copy()
+            #print("Gamma noise for: " + str(image_file))
+            for scale_parameter in scale_parameters:
+                # Add gamma-distributed noise
+                
+                gamma_noisy_image = crop_img_clean.copy()
+                gamma_noise = np.random.gamma(shape=2, scale=scale_parameter, size=image.shape[:2])
+                gamma_noise = np.repeat(gamma_noise[:, :, np.newaxis], 3, axis=2).astype(np.uint8)
+                gamma_noisy_image = cv2.add(image, gamma_noise)
+                #print(Path(image_file).stem)
+                # Save clean and noisy images in separate folders
+                clean_image_output_path = os.path.join(output_folder_clean, f"{Path(image_file).stem}_gamma_{scale_parameter}.png")
+                noisy_image_output_path = os.path.join(output_folder_noisy, f"{Path(image_file).stem}_gamma_{scale_parameter}.png")
+                #print(clean_image_output_path)
+                cv2.imwrite(clean_image_output_path, image)
+                cv2.imwrite(noisy_image_output_path, gamma_noisy_image)
+
+
+def add_gaussian_noise_to_images(input_folder, output_folder_clean, output_folder_noisy, sigma_parameters):
+    """
+    Read images from input_folder, add Gaussian noise with different sigma parameters,
+    and save noisy and clean versions in output_folder_noisy and output_folder_clean, respectively.
+
+    Parameters:
+    - input_folder: Path to the folder containing input images.
+    - output_folder_clean: Path to the folder where clean images will be saved.
+    - output_folder_noisy: Path to the folder where noisy images will be saved.
+    - sigma_parameters: List of standard deviations (sigma) for Gaussian noise.
+    """
+    # Create output folders if they don't exist
+    os.makedirs(output_folder_clean, exist_ok=True)
+    os.makedirs(output_folder_noisy, exist_ok=True)
+
+    # List all files in the input folder
+    image_files = list_image_files(input_folder)
+    y=0
+    x=0
+    h=600
+    w=1024
+    for image_file in image_files:
+        if image_file.endswith(('.jpg', '.jpeg', '.png')):
+            # Read the image
+            #image_path = os.path.join(input_folder, image_file)
+            image = cv2.imread(image_file)
+            crop_img_clean = image[y:y+h, x:x+w]
+            image = crop_img_clean.copy()
+            #print("Gaussian noise for : " + str(image_file))
+            for sigma_parameter in sigma_parameters:
+                # Add Gaussian noise
+                
+                gaussian_noisy_image = image.copy()
+                gaussian_noise = np.random.normal(0, sigma_parameter, image.shape).astype(np.uint8)
+                gaussian_noisy_image = cv2.add(image, gaussian_noise)
+
+                # Save clean and noisy images in separate folders
+                clean_image_output_path = os.path.join(output_folder_clean, f"{Path(image_file).stem}_gaussian_{sigma_parameter}.png")
+                noisy_image_output_path = os.path.join(output_folder_noisy, f"{Path(image_file).stem}_gaussian_{sigma_parameter}.png")
+
+                cv2.imwrite(clean_image_output_path, image)
+                cv2.imwrite(noisy_image_output_path, gaussian_noisy_image)
+
+# Function to recursively list image files in a directory
+def list_image_files(root_dir):
+    image_files = []
+    for root, _, files in os.walk(root_dir):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                image_files.append(os.path.join(root, file))
+    image_files_n = random.choices(image_files, k=100)
+    return image_files_n
+
+if __name__ == "__main__":
+
+    input_folder = "clean"
+    output_folder_clean_gamma = "dataset_denoise/gamma/clean"
+    output_folder_noisy_gamma = "dataset_denoise/gamma/noisy"
+    output_folder_clean_gaussian = "dataset_denoise/gaussian/clean"
+    output_folder_noisy_gaussian = "dataset_denoise/gaussian/noisy"
+    sigma_parameters = [5,10,15,20,25,30,35,40,45,50]
+    scale_parameters = [5,10,15,20,25,30,35,40,45,50]
+    #scale_parameters = [1.0, 2.0, 5.0]  # List of gamma scale parameters
+    #sigma_parameters = [10, 20, 30]     # List of Gaussian standard deviations
+    print("Gamma Noise")
+    add_gamma_noise_to_images(input_folder, output_folder_clean_gamma, output_folder_noisy_gamma, scale_parameters)
+    print("Gaussian Noise")
+    add_gaussian_noise_to_images(input_folder, output_folder_clean_gaussian, output_folder_noisy_gaussian, sigma_parameters)
---
+++ datasets/denoising_test/unet.py
@@ -0,0 +1,152 @@
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+from sklearn.model_selection import train_test_split
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
+from skimage.metrics import structural_similarity as compare_ssim
+
+
+# Define the U-Net architecture
+def unet(input_size=(256, 256, 1)):
+    inputs = Input(input_size)
+
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+
+    up6 = UpSampling2D(size=(2, 2))(conv5)
+    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up6)
+    merge6 = concatenate([conv4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+    up7 = UpSampling2D(size=(2, 2))(conv6)
+    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up7)
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+    up8 = UpSampling2D(size=(2, 2))(conv7)
+    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up8)
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+    up9 = UpSampling2D(size=(2, 2))(conv8)
+    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up9)
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+
+    conv10 = Conv2D(1, 1, activation='linear')(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+
+    return model
+
+# Data loading and preprocessing
+# Data loading and preprocessing
+def load_data(data_directory, img_size=(256, 256)):
+    clean_images = []
+    noisy_images = []
+    clean_directory = os.path.join(data_directory, 'clean')
+    noisy_directory = os.path.join(data_directory, 'noisy')
+    for img_file in os.listdir(clean_directory):
+        clean_img = load_img(os.path.join(clean_directory, img_file), color_mode="grayscale", target_size=img_size)
+        clean_img_array = img_to_array(clean_img) / 255.0  # Normalize to [0, 1]
+        clean_images.append(clean_img_array)
+
+        noisy_img = load_img(os.path.join(noisy_directory, img_file), color_mode="grayscale", target_size=img_size)
+        noisy_img_array = img_to_array(noisy_img) / 255.0  # Normalize to [0, 1]
+        noisy_images.append(noisy_img_array)
+
+    return np.array(clean_images), np.array(noisy_images)
+
+# Load your dataset
+data_directory = 'dataset_denoise/gamma'
+clean_images, noisy_images = load_data(data_directory)
+
+
+train_clean, temp_clean, train_noisy, temp_noisy = train_test_split(clean_images, noisy_images, test_size=0.2, random_state=42)
+test_clean, val_clean, test_noisy, val_noisy = train_test_split(temp_clean, temp_noisy, test_size=0.5, random_state=42)
+
+# Compile the model
+model = unet()
+model.compile(optimizer=Adam(learning_rate=1e-4), loss='mean_squared_error', metrics=['mean_squared_error'])
+
+# Train the model
+checkpoint = ModelCheckpoint('unet_denoising_weights.h5', monitor='val_loss', save_best_only=True)
+callbacks_list = [checkpoint]
+
+# Fit the model
+model.fit(train_noisy, train_clean, batch_size=32, epochs=20, verbose=1, validation_data=(val_noisy, val_clean), callbacks=callbacks_list)
+
+# Evaluate the model on the test data
+loss, mse = model.evaluate(test_noisy, test_clean, verbose=1)
+print(f'Test loss: {loss}, Test Mean Squared Error: {mse}')
+
+
+
+# Assuming the model has already been loaded
+# Assuming test_noisy and test_clean are already available
+
+# Denoise the test data
+denoised_images = model.predict(test_noisy)
+
+# Evaluate denoised images
+psnr_values = []
+ssim_values = []
+
+for i in range(len(test_clean)):
+    psnr = compare_psnr(test_clean[i], denoised_images[i])
+    ssim = compare_ssim(test_clean[i], denoised_images[i])
+    # Convert the denoised image array to an image and save it
+    denoised_img = array_to_img(denoised_images[i] * 255.0, scale=False)
+    denoised_img.save('den/gamma/denoised__gamma_image_'+str(i)+'.png')
+    psnr_values.append(psnr)
+    ssim_values.append(ssim)
+
+# Compute average PSNR and SSIM
+avg_psnr = sum(psnr_values) / len(psnr_values)
+avg_ssim = sum(ssim_values) / len(ssim_values)
+
+print(f'Average PSNR: {avg_psnr}')
+print(f'Average SSIM: {avg_ssim}')
+
+
+# Test the model on test data
+#test_img = load_img('path_to_test_image', color_mode="grayscale", target_size=(256, 256))
+#test_img_array = img_to_array(test_img) / 255.0
+#test_img_array = np.expand_dims(test_img_array, axis=0)
+
+# Assuming you have already loaded the model
+# model = tf.keras.models.load_model('unet_denoising_model.h5')
+#denoised_img_array = model.predict(test_img_array)
+
+# Convert the denoised image array to an image and save it
+#denoised_img = array_to_img(denoised_img_array[0] * 255.0, scale=False)
+#denoised_img.save('denoised__gamma_image.png')
+
+# Save the model
+model.save('unet_denoising_gamma_model.h5')
+

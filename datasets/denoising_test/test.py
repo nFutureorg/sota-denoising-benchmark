77279108ff5e0224d4926075bdb879cada0bf881
Sheikh Shah Mohammad Motiur Rahman <motiur.ion@gmail.com>
2023-10-25 16:38:56 +0200

    Update test.py

--- datasets/denoising_test/test.py
+++ datasets/denoising_test/test.py
@@ -1,18 +1,10 @@
-import numpy as np
-import os
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
-from sklearn.model_selection import train_test_split
+import numpy as np
 from skimage.metrics import peak_signal_noise_ratio as compare_psnr
 from skimage.metrics import structural_similarity as compare_ssim
-import sys
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
 
-
-# Test the model on test data
+# Load the test image
 test_img = load_img('SEM/pollen.spd3.0098.png', target_size=(256, 256))
 test_img_array = img_to_array(test_img) / 255.0
 test_img_array = np.expand_dims(test_img_array, axis=0)
@@ -25,8 +17,11 @@ denoised_img_array = model.predict(test_img_array)
 denoised_img = array_to_img(denoised_img_array[0] * 255.0, scale=False)
 denoised_img.save('denoised_pollen_image.png')
 
-psnr = compare_psnr(test_img, denoised_img)
-ssim = compare_ssim(test_img, denoised_img,channel_axis=2)
+# Calculate PSNR and SSIM
+test_img_array = np.squeeze(test_img_array, axis=0)
+denoised_img_array = np.squeeze(denoised_img_array, axis=0)
+psnr = compare_psnr(test_img_array, denoised_img_array)
+ssim = compare_ssim(test_img_array, denoised_img_array, multichannel=True)
 
-print(f'PSNR: {psnr}')
-print(f'SSIM: {ssim}')
+print(f"PSNR: {psnr}, SSIM: {ssim}")
+
